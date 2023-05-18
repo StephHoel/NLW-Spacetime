e69b28e1853b193f15696d5f178d382b086bf12f
Steph Hoel <steph.hoel@gmail.com>
2023-05-18 19:15:57 -0300

    Adding JWT

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,12 +4,17 @@ import fastify from 'fastify'
 import { memoriesRoutes } from './routes/memories'
 import cors from '@fastify/cors'
 import { authRoutes } from './routes/auth'
+import jwt from '@fastify/jwt'
 
 const app = fastify()
 
 app.register(cors, {
   // origin: true, // qualquer caminho pode acessar
   origin: ['http://localhost:3000'],
+})
+
+app.register(jwt, {
+  secret: 'spacetime',
 })
 
 app.register(authRoutes)
