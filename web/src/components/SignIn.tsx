e6b9ebfd720e227d97ac8ad8f61e9c89d00d28b0
Steph Hoel <steph.hoel@gmail.com>
2023-05-19 16:46:43 -0300

    Alter url to SignIn

--- web/src/components/SignIn.tsx
+++ web/src/components/SignIn.tsx
@@ -1,9 +1,12 @@
 import { User } from 'lucide-react'
+import Link from 'next/link'
+
+import { signInUrl } from '@/lib/api'
 
 export function SignIn() {
   return (
-    <a
-      href={`https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`}
+    <Link
+      href={signInUrl}
       className="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
     >
       <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
@@ -13,6 +16,6 @@ export function SignIn() {
       <p className="max-w-[140px] text-sm leading-snug">
         <span className="underline">Crie sua conta</span> e salve suas memórias
       </p>
-    </a>
+    </Link>
   )
 }
