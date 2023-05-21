48d2815cc4c5b8bd6f5f99f9513e4e07cc072e91
Steph Hoel <steph.hoel@gmail.com>
2023-05-21 08:25:13 -0300

    Add Feature to add new memory (redirect to new page) on Mobile

--- mobile/app/memories.tsx
+++ mobile/app/memories.tsx
@@ -1,9 +1,32 @@
-import { View, Text } from 'react-native'
+import { View, TouchableOpacity, ScrollView } from 'react-native'
 
-export default function Memories() {
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+
+import { Link } from 'expo-router'
+
+import Icon from '@expo/vector-icons/Feather'
+
+import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
+
+export default function NewMemory() {
+  const { bottom, top } = useSafeAreaInsets()
+
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text>Memories</Text>
-    </View>
+    <ScrollView
+      className="flex-1 px-8"
+      contentContainerStyle={{ paddingBottom: bottom, paddingTop: top }}
+    >
+      <View className="flex-row items-center justify-between">
+        <NLWLogo />
+
+        <View className="flex-row gap-2">
+          <Link href="/new" asChild>
+            <TouchableOpacity className="h-10 w-10 items-center justify-center rounded-full bg-green-500">
+              <Icon name="plus" size={16} color="#000" />
+            </TouchableOpacity>
+          </Link>
+        </View>
+      </View>
+    </ScrollView>
   )
 }
