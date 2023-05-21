1838f0f06684758a9f820e8dd3c8717a1e3a576c
Steph Hoel <steph.hoel@gmail.com>
2023-05-21 08:28:13 -0300

    Add Feature Logout in Memories Page (Mobile)

--- mobile/app/memories.tsx
+++ mobile/app/memories.tsx
@@ -2,7 +2,9 @@ import { View, TouchableOpacity, ScrollView } from 'react-native'
 
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
-import { Link } from 'expo-router'
+import { Link, useRouter } from 'expo-router'
+
+import * as SecureStore from 'expo-secure-store'
 
 import Icon from '@expo/vector-icons/Feather'
 
@@ -10,6 +12,14 @@ import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
 
 export default function NewMemory() {
   const { bottom, top } = useSafeAreaInsets()
+
+  const router = useRouter()
+
+  async function signOut() {
+    await SecureStore.deleteItemAsync('token')
+
+    router.push('/')
+  }
 
   return (
     <ScrollView
@@ -25,6 +35,13 @@ export default function NewMemory() {
               <Icon name="plus" size={16} color="#000" />
             </TouchableOpacity>
           </Link>
+
+          <TouchableOpacity
+            onPress={signOut}
+            className="h-10 w-10 items-center justify-center rounded-full bg-red-500"
+          >
+            <Icon name="log-out" size={16} color="#000" />
+          </TouchableOpacity>
         </View>
       </View>
     </ScrollView>
