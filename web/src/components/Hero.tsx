2d9f702d48f4260ce1dbfaeb51435bd2fb0ba4d4
Steph Hoel <steph.hoel@gmail.com>
2023-05-24 09:29:26 -0300

    Adding link in logo

--- web/src/components/Hero.tsx
+++ web/src/components/Hero.tsx
@@ -6,7 +6,9 @@ import nlwLogo from '../assets/nlw-spacetime-logo.svg'
 export function Hero() {
   return (
     <div className="space-y-5">
-      <Image src={nlwLogo} alt="NLW Spacetime" />
+      <Link href="/">
+        <Image src={nlwLogo} alt="NLW Spacetime" />
+      </Link>
 
       <div className="max-w-[420px] space-y-1">
         <h1 className="text-5xl font-bold leading-tight text-gray-50">
